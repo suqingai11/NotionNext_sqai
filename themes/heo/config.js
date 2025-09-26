eb5e04ec3352777149657cb99049df79dfe6f9bc
suqingai11 <858438176@qq.com>
2025-09-26 11:12:08 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,11 +8,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    // { title: '欢迎小范美女来到我的博客', url: 'https://blog.tangly1024.com' },
-    // { title: '欢迎小范美女来到我的博客', url: 'https://docs.tangly1024.com' }
-
-     { title: '欢迎来到我的博客' },
-    { title: '欢迎来到我的博客'}
+  { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
+  { title: '欢迎来到我的博客', url: 'https://docs.tangly1024.com' }
   ],
 
   // 英雄区左右侧组件颠倒位置

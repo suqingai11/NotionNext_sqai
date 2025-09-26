74263eaa26d678457967b00ece7a90de18a37d31
suqingai11 <858438176@qq.com>
2025-09-26 10:23:55 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -4,33 +4,36 @@ const CONFIG = {
 
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2025-09-24', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎小范美女来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '欢迎小范美女来到我的博客', url: 'https://docs.tangly1024.com' }
+    // { title: '欢迎小范美女来到我的博客', url: 'https://blog.tangly1024.com' },
+    // { title: '欢迎小范美女来到我的博客', url: 'https://docs.tangly1024.com' }
+
+     { title: '欢迎来到我的博客' },
+    { title: '欢迎来到我的博客'}
   ],
 
   // 英雄区左右侧组件颠倒位置
-  HEO_HERO_REVERSE: false,
+  HEO_HERO_REVERSE: true,
   // 博客主体区左右侧组件颠倒位置
-  HEO_HERO_BODY_REVERSE: false,
+  HEO_HERO_BODY_REVERSE: true,
 
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: 'AI创作',
-  HEO_HERO_TITLE_2: '与思维认知',
+ // HEO_HERO_TITLE_2: '与思维认知',
   HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '未设定',
+  HEO_HERO_TITLE_4: '爆款模型推荐',
   HEO_HERO_TITLE_5: '未设定',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  //HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
   // 英雄区遮罩文字
-  HEO_HERO_COVER_TITLE: '随便逛逛',
+  //HEO_HERO_COVER_TITLE: '随便逛逛',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '精选模型', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门工作流', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '精选模型', url: '/tag/精选模型' },
+  HEO_HERO_CATEGORY_2: { title: '热门工作流', url: '/tag/热门工作流' },
+  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程  ' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
@@ -49,12 +52,12 @@ const CONFIG = {
   ],
 
   // 个人资料底部按钮
-  HEO_INFO_CARD_URL1: '/about',
-  HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
-  HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
-  HEO_INFO_CARD_TEXT3: '了解更多',
+  // HEO_INFO_CARD_URL1: '/about',
+  // HEO_INFO_CARD_ICON1: 'fas fa-user',
+  // HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
+  // HEO_INFO_CARD_ICON2: 'fab fa-github',
+  //HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
+  //HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
@@ -120,13 +123,13 @@ const CONFIG = {
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  //HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
 
   // 底部统计面板文案
-  HEO_POST_COUNT_TITLE: '文章数:',
-  HEO_SITE_TIME_TITLE: '建站天数:',
-  HEO_SITE_VISIT_TITLE: '访问量:',
-  HEO_SITE_VISITOR_TITLE: '访客数:',
+  //HEO_POST_COUNT_TITLE: '文章数:',
+  //HEO_SITE_TIME_TITLE: '建站天数:',
+  //HEO_SITE_VISIT_TITLE: '访问量:',
+  //HEO_SITE_VISITOR_TITLE: '访客数:',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
